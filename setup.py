a2e0df8a33cba9ec470f4e6c06ec5c685ed79e50
Nick Tustison <ntustison@gmail.com>
2019-06-04 14:57:51 -0700

    ENH:  reconfiguration for packaging.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='antspynet',
+      version='0.0',
+      description='A collection of deep learning architectures ported to the python language and tools for basic medical image processing.',
+      url='http://github.com/storborg/funniest',
+      author='Nicholas J. Tustison and Brian B. Avants',
+      author_email='ntustison@gmail.com',
+      packages=['antspynet'],
+      install_requires=['antspyx'],
+      zip_safe=False)
