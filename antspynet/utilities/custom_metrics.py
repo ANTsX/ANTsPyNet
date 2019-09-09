cb44411350b895e5408ebca02d561927240ec455
Nick Tustison <ntustison@gmail.com>
2019-09-09 13:49:30 -0700

    BUG:  Minor fixes.

--- antspynet/utilities/custom_metrics.py
+++ antspynet/utilities/custom_metrics.py
@@ -66,28 +66,25 @@ def pearson_correlation_coefficient(y_true, y_pred):
 
     return(coefficient)
 
-def categorical_focal_gain(y_true, y_pred, gamma=2.0, alpha=0.25)
-{
-  def categorical_focal_gain_fixed(y_true, y_pred)
-    {
-    y_pred = y_pred / K.sum(y_pred, axis=-1L, keepdims=True)
-    y_pred = K.clip(y_pred, K.epsilon(), 1.0 - K.epsilon())
-    cross_entropy = y_true * K.log(y_pred)
-    gain = alpha * K.pow(1.0 - y_pred, gamma) * cross_entropy
-    return(K.sum(gain, axis=-1L))
-    }
-  return(categorical_focal_gain_fixed)
-}
+def categorical_focal_gain(y_true, y_pred, gamma=2.0, alpha=0.25):
 
-def categorical_focal_loss(y_true, y_pred, gamma=2.0, alpha=0.25)
-{
-  def categorical_focal_loss_fixed(y_true, y_pred)
-    {
-    y_pred = y_pred / K.sum(y_pred, axis=-1L, keepdims=True)
-    y_pred = K.clip(y_pred, K.epsilon(), 1.0 - K.epsilon())
-    cross_entropy = y_true * K.log(y_pred)
-    loss = alpha * K.pow(1.0 - y_pred, gamma) * cross_entropy
-    return(-K.sum(loss, axis=-1L))
-    }
-  return(categorical_focal_loss_fixed)
-}
+    def categorical_focal_gain_fixed(y_true, y_pred):
+        y_pred = y_pred / K.sum(y_pred, axis=-1, keepdims=True)
+        y_pred = K.clip(y_pred, K.epsilon(), 1.0 - K.epsilon())
+        cross_entropy = y_true * K.log(y_pred)
+        gain = alpha * K.pow(1.0 - y_pred, gamma) * cross_entropy
+        return(K.sum(gain, axis=-1))
+
+    return(categorical_focal_gain_fixed)
+
+
+def categorical_focal_loss(y_true, y_pred, gamma=2.0, alpha=0.25):
+
+    def categorical_focal_loss_fixed(y_true, y_pred):
+        y_pred = y_pred / K.sum(y_pred, axis=-1, keepdims=True)
+        y_pred = K.clip(y_pred, K.epsilon(), 1.0 - K.epsilon())
+        cross_entropy = y_true * K.log(y_pred)
+        loss = alpha * K.pow(1.0 - y_pred, gamma) * cross_entropy
+        return(-K.sum(loss, axis=-1))
+
+    return(categorical_focal_loss_fixed)
--- antspynet/utilities/resample_tensor_utilities.py
+++ antspynet/utilities/resample_tensor_utilities.py
@@ -27,13 +27,13 @@ class ResamplingLayer2D(Layer):
     def __init__(self, shape, interpolation_type='nearest_neighbor', name='', **kwargs):
 
         if len(shape) != 2:
-            ValueError("shape must be of length 2 specifying the width and " +
-                       "height of the resampled tensor.")
+            raise ValueError("shape must be of length 2 specifying the width and " +
+                             "height of the resampled tensor.")
         self.shape = shape
 
         allowed_types = set(['nearest_neighbor', 'linear', 'cubic'])
         if interpolation_type in allowed_types:
-            ValueError("interpolation_type not one of the allowed types.")
+            raise ValueError("interpolation_type not one of the allowed types.")
         self.interpolation_type = interpolation_type
 
         self.name = name
@@ -42,8 +42,8 @@ class ResamplingLayer2D(Layer):
 
     def compute_output_shape(input_shape):
         if len(input_shape) != 4:
-            ValueError("Input tensor must be of rank 4.")
-        return([input_shape[0], self.shape[0], self.shape[2], input_shape[3])
+            raise ValueError("Input tensor must be of rank 4.")
+        return([input_shape[0], self.shape[0], self.shape[2], input_shape[3]])
 
     def call(self, x, mask=None):
         dimensionality = 2
@@ -52,18 +52,18 @@ class ResamplingLayer2D(Layer):
         input_shape = x.get_shape()
         old_size=input_shape[1:dimensionality]
 
-      if new_size == old_size:
-          return(x + 0)
+        if new_size == old_size:
+            return(x + 0)
 
-      resampled_tensor = None
-      if self.interpolation_type == 'nearestNeighbor':
-          resampled_tensor = tf.image.resize_nearest_neighbor(x, size=new_size, align_corners=True)
-          elif self.interpolation_type == 'linear':
-          resampled_tensor = tf.image.resize_bilinear(x, size=new_size, align_corners=True)
-          elif self.interpolation_type == 'cubic':
-          resampled_tensor = tf.image.resize_bicubic(x, size=new_size, align_corners=True)
+        resampled_tensor = None
+        if self.interpolation_type == 'nearestNeighbor':
+            resampled_tensor = tf.image.resize_nearest_neighbor(x, size=new_size, align_corners=True)
+        elif self.interpolation_type == 'linear':
+            resampled_tensor = tf.image.resize_bilinear(x, size=new_size, align_corners=True)
+        elif self.interpolation_type == 'cubic':
+            resampled_tensor = tf.image.resize_bicubic(x, size=new_size, align_corners=True)
 
-      return(resampled_tensor)
+        return(resampled_tensor)
 
     def get_config(self):
         config = {"shape": self.shape, "interpolation_type": self.interpolation_type}
@@ -93,13 +93,13 @@ class ResamplingLayer3D(Layer):
     def __init__(self, shape, interpolation_type='nearest_neighbor', name='', **kwargs):
 
         if len(shape) != 3:
-            ValueError("shape must be of length 3 specifying the width, " +
-                       "height, and depth of the resampled tensor.")
+            raise ValueError("shape must be of length 3 specifying the width, " +
+                             "height, and depth of the resampled tensor.")
         self.shape = shape
 
         allowed_types = set(['nearest_neighbor', 'linear', 'cubic'])
         if interpolation_type in allowed_types:
-            ValueError("interpolation_type not one of the allowed types.")
+            raise ValueError("interpolation_type not one of the allowed types.")
         self.interpolation_type = interpolation_type
 
         self.name = name
@@ -108,9 +108,9 @@ class ResamplingLayer3D(Layer):
 
     def compute_output_shape(input_shape):
         if len(input_shape) != 5:
-            ValueError("Input tensor must be of rank 5.")
+            raise ValueError("Input tensor must be of rank 5.")
         return([input_shape[0], self.shape[0], self.shape[2], self.shape[3],
-                input_shape[4])
+                input_shape[4]])
 
     def call(self, x, mask=None):
         dimensionality = 3
@@ -126,26 +126,22 @@ class ResamplingLayer3D(Layer):
         resampled_tensor = None
 
         # Do yz
-        squeeze_tensor_yz =
-          tf.reshape(x, (-1, old_size[1], old_size[2], channel_size))
+        squeeze_tensor_yz = tf.reshape(x, (-1, old_size[1], old_size[2], channel_size))
 
         new_shape_yz = (new_size[1], new_size[2])
 
         resampled_tensor_yz = None
         if self.interpolation_type == 'nearest_neighbor':
-            resampled_tensor_yz =
-              tf.image.resize_nearest_neighbor(squeeze_tensor_yz,
-                size=new_shape_yz, align_corners=True)
+            resampled_tensor_yz = tf.image.resize_nearest_neighbor(squeeze_tensor_yz,
+              size=new_shape_yz, align_corners=True)
         elif self.interpolation_type == 'linear':
-            resampled_tensor_yz =
-              tf.image.resize_bilinear(squeeze_tensor_yz,
-                size=new_shape_yz, align_corners=True)
+            resampled_tensor_yz = tf.image.resize_bilinear(squeeze_tensor_yz,
+              size=new_shape_yz, align_corners=True)
         elif self.interpolation_type == 'cubic':
-            resampled_tensor_yz =
-              tf.image.resize_bicubic(squeeze_tensor_yz,
-                size=new_shape_yz, align_corners=True)
+            resampled_tensor_yz = tf.image.resize_bicubic(squeeze_tensor_yz,
+              size=new_shape_yz, align_corners=True)
 
-        new_shape_yz = (-1L, old_size[0], new_size[1], new_size[2], channel_size)
+        new_shape_yz = (-1, old_size[0], new_size[1], new_size[2], channel_size)
         resume_tensor_yz = tf.reshape(resampled_tensor_yz, new_shape_yz)
 
         # Do x
@@ -159,19 +155,16 @@ class ResamplingLayer3D(Layer):
 
         resampled_tensor_x = NULL
         if self.interpolation_type == 'nearest_neighbor':
-            resampled_tensor_x =
-              tf.image.resize_nearest_neighbor(squeeze_tensor_x,
-                size=new_shape_x, align_corners=True )
+            resampled_tensor_x = tf.image.resize_nearest_neighbor(squeeze_tensor_x,
+                size=new_shape_x, align_corners=True)
         elif self.interpolation_type == 'linear':
-            resampled_tensor_x =
-              tf.image.resize_bilinear(squeeze_tensor_x,
+            resampled_tensor_x = tf.image.resize_bilinear(squeeze_tensor_x,
                 size=new_shape_x, align_corners=True)
         elif self.interpolation_type == 'cubic':
-            resampled_tensor_x =
-              tf.image.resize_bicubic(squeeze_tensor_x,
+            resampled_tensor_x = tf.image.resize_bicubic(squeeze_tensor_x,
                 size=new_shape_x, align_corners=True)
 
-        new_shape_x = (-1, new_size[2], new_size[1], new_size[0], channel_size )
+        new_shape_x = (-1, new_size[2], new_size[1], new_size[0], channel_size)
         resumeTensor_x = tf.reshape(resampled_tensor_x, new_shape_x)
 
         resampled_tensor = tf.transpose(resumeTensor_x, (0, 3, 2, 1, 4))
