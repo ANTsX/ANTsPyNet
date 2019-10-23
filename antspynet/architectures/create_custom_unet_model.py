804544a6d2636a28fffd7edcff4a57b079d0459e
Nick Tustison <ntustison@gmail.com>
2019-10-23 10:36:06 -0700

    ENH:  Clean up.

--- antspynet/architectures/create_custom_unet_model.py
+++ antspynet/architectures/create_custom_unet_model.py
@@ -28,50 +28,8 @@ def create_nobrainer_unet_model_3d(input_image_size):
 
     Example
     -------
-    >>> import requests
-    >>> import tempfile
-    >>> from os import path
-    >>> import ants
-    >>> import numpy as np
-    >>>
     >>> model = create_nobrainer_unet_model_3d((None, None, None, 1))
     >>> model.summary()
-    >>>
-    >>> url_weights = "https://github.com/neuronets/nobrainer-models/releases/download/0.1/brain-extraction-unet-128iso-weights.h5"
-    >>> temp_directory = tempfile.TemporaryDirectory()
-    >>> target_file_weights = tempfile.NamedTemporaryFile(suffix=".h5", dir=temp_directory.name)
-    >>> target_file_weights.close()
-    >>> target_file_weights_name = target_file_weights.name
-    >>> if not path.exists(target_file_weights_name):
-    >>>     r = requests.get(url_weights)
-    >>>     with open(target_file_weights_name, 'wb') as f:
-    >>>         f.write(r.content)
-    >>>
-    >>> model.load_weights(target_file_weights_name)
-    >>>
-    >>> url_image = "https://github.com/ANTsXNet/BrainExtraction/blob/master/Data/Example/1097782_defaced_MPRAGE.nii.gz?raw=true"
-    >>> target_file_image = tempfile.NamedTemporaryFile(suffix=".nii.gz", dir=temp_directory.name)
-    >>> target_file_image.close()
-    >>> target_file_image_name = target_file_image.name
-    >>> if not path.exists(target_file_image_name):
-    >>>     r = requests.get(url_image)
-    >>>     with open(target_file_image_name, 'wb') as f:
-    >>>         f.write(r.content)
-    >>>
-    >>> image = ants.image_read(target_file_image_name)
-    >>> image = ants.image_math(image, 'Normalize') * 255.0
-    >>> image_resampled = ants.resample_image(image, (256, 256, 256), True)
-    >>> batchX = np.expand_dims(image_resampled.numpy(), axis=0)
-    >>> batchX = np.expand_dims(batchX, axis=-1)
-    >>>
-    >>> brain_mask_array = model.predict(batchX, verbose=0)
-    >>> brain_mask_resampled = ants.from_numpy(np.squeeze(brain_mask_array[0,:,:,:,0]),
-          origin=image_resampled.origin, spacing=image_resampled.spacing,
-          direction=image_resampled.direction)
-    >>> brain_mask_image = ants.resample_image(brain_mask_resampled, image.shape, True, 1)
-    >>> minimum_brain_volume = round( 649933.7 )
-    >>> brain_mask_labeled = ants.label_clusters(brain_mask_image, minimum_brain_volume)
-    >>> ants.image_write(brain_mask_labeled, "brain_mask.nii.gz")
     """
 
     number_of_outputs = 1
