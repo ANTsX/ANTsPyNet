f84f25b2028319d5db50d5729e3679d2db6d9e8d
Nick Tustison <ntustison@gmail.com>
2020-06-20 21:30:04 -0700

    ENH:  Update weights.

--- antspynet/utilities/get_pretrained_network.py
+++ antspynet/utilities/get_pretrained_network.py
@@ -49,7 +49,7 @@ def get_pretrained_network(file_id=None, target_file_name=None):
             "dbpn4x": "https://ndownloader.figshare.com/files/13347617",
             "deepFlash": "https://ndownloader.figshare.com/files/22933757",
             "denoising": "https://ndownloader.figshare.com/files/14235296",
-            "dktInner": "https://ndownloader.figshare.com/files/23241827",
+            "dktInner": "https://ndownloader.figshare.com/files/23266943",
             "functionalLungMri": "https://ndownloader.figshare.com/files/13824167",
             "hippMapp3rInitial": "https://ndownloader.figshare.com/files/18068408",
             "hippMapp3rRefine": "https://ndownloader.figshare.com/files/18068411",
