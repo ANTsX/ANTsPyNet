d999c561d192e8cd73bc70efc1bace35cf743603
Nick Tustison <ntustison@gmail.com>
2020-12-23 19:45:28 -0800

    ENH:  Add nn-unet activaiton style.

--- antspynet/architectures/create_unet_model.py
+++ antspynet/architectures/create_unet_model.py
@@ -1,11 +1,13 @@
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import (Input, Concatenate, Dropout, Add, Activation,
-                                     multiply, ReLU,
+                                     multiply, ReLU, LeakyReLU,
                                      Conv2D, Conv2DTranspose, MaxPooling2D,
                                      UpSampling2D,
                                      Conv3D, Conv3DTranspose, MaxPooling3D,
                                      UpSampling3D)
 from tensorflow.keras import regularizers
+
+from ..utilities import InstanceNormalization
 
 def create_unet_model_2d(input_image_size,
                          number_of_outputs=2,
@@ -17,8 +19,8 @@ def create_unet_model_2d(input_image_size,
                          strides=(2, 2),
                          dropout_rate=0.0,
                          weight_decay=0.0,
+                         nn_unet_activation_style=False,
                          add_attention_gating=False,
-                         activation_type = 'relu',
                          mode='classification'
                         ):
     """
@@ -77,8 +79,9 @@ def create_unet_model_2d(input_image_size,
         Weighting parameter for L2 regularization of the kernel weights of the
         convolution layers.  Default = 0.0.
 
-    activation_type :  string
-        Activation type at each convolutional layer; default relu.
+    nn_unet_activation_style : boolean
+        Instance normalization followed by leaky relu Activation on the convolution
+        layers.
 
     add_attention_gating :  boolean
         Whether or not to include attention gating.
@@ -96,6 +99,11 @@ def create_unet_model_2d(input_image_size,
     >>> model = create_unet_model_2d((128, 128, 1))
     >>> model.summary()
     """
+
+    def nn_unet_activation(x):
+        x = InstanceNormalization()(x)
+        x = LeakyReLU(alpha=0.01)(x)
+        return x
 
     def attention_gate_2d(x, g, inter_shape):
         x_theta = Conv2D(filters=inter_shape,
@@ -125,23 +133,32 @@ def create_unet_model_2d(input_image_size,
         if i == 0:
             conv = Conv2D(filters=number_of_filters,
                           kernel_size=convolution_kernel_size,
-                          activation=activation_type,
                           padding='same',
                           kernel_regularizer=regularizers.l2(weight_decay))(inputs)
         else:
             conv = Conv2D(filters=number_of_filters,
                           kernel_size=convolution_kernel_size,
-                          activation=activation_type,
                           padding='same',
                           kernel_regularizer=regularizers.l2(weight_decay))(pool)
+
+        if nn_unet_activation_style == True:
+            conv = nn_unet_activation(conv)
+        else:
+            conv = ReLU()(conv)
 
         if dropout_rate > 0.0:
             conv = Dropout(rate=dropout_rate)(conv)
 
-        encoding_convolution_layers.append(Conv2D(filters=number_of_filters,
-                                                  kernel_size=convolution_kernel_size,
-                                                  activation=activation_type,
-                                                  padding='same')(conv))
+        conv = Conv2D(filters=number_of_filters,
+                      kernel_size=convolution_kernel_size,
+                      padding='same')(conv)
+
+        if nn_unet_activation_style == True:
+            conv = nn_unet_activation(conv)
+        else:
+            conv = ReLU()(conv)
+
+        encoding_convolution_layers.append(conv)
 
         if i < number_of_layers - 1:
             pool = MaxPooling2D(pool_size=pool_size)(encoding_convolution_layers[i])
@@ -155,6 +172,8 @@ def create_unet_model_2d(input_image_size,
                                  kernel_size=deconvolution_kernel_size,
                                  padding='same',
                                  kernel_regularizer=regularizers.l2(weight_decay))(outputs)
+        if nn_unet_activation_style == True:
+            deconv = nn_unet_activation(deconv)
         deconv = UpSampling2D(size=pool_size)(deconv)
 
         if add_attention_gating == True:
@@ -167,18 +186,25 @@ def create_unet_model_2d(input_image_size,
 
         outputs = Conv2D(filters=number_of_filters,
                          kernel_size=convolution_kernel_size,
-                         activation=activation_type,
                          padding='same',
                          kernel_regularizer=regularizers.l2(weight_decay))(outputs)
+        if nn_unet_activation_style == True:
+            outputs = nn_unet_activation(outputs)
+        else:
+            outputs = ReLU()(outputs)
 
         if dropout_rate > 0.0:
             outputs = Dropout(rate=dropout_rate)(outputs)
 
         outputs = Conv2D(filters=number_of_filters,
                          kernel_size=convolution_kernel_size,
-                         activation=activation_type,
                          padding='same',
                          kernel_regularizer=regularizers.l2(weight_decay))(outputs)
+
+        if nn_unet_activation_style == True:
+            outputs = nn_unet_activation(outputs)
+        else:
+            outputs = ReLU()(outputs)
 
     convActivation = ''
 
@@ -209,7 +235,7 @@ def create_unet_model_3d(input_image_size,
                          strides=(2, 2, 2),
                          dropout_rate=0.0,
                          weight_decay=0.0,
-                         activation_type='relu',
+                         nn_unet_activation_style=False,
                          add_attention_gating=False,
                          mode='classification'
                         ):
@@ -269,8 +295,9 @@ def create_unet_model_3d(input_image_size,
         Weighting parameter for L2 regularization of the kernel weights of the
         convolution layers.  Default = 0.0.
 
-    activation_type :  string
-        Activation type at each convolutional layer; default relu.
+    nn_unet_activation_style : boolean
+        Instance normalization followed by leaky relu Activation on the convolution
+        layers.
 
     add_attention_gating :  boolean
         Whether or not to include attention gating.
@@ -288,6 +315,11 @@ def create_unet_model_3d(input_image_size,
     >>> model = create_unet_model_3d((128, 128, 128, 1))
     >>> model.summary()
     """
+
+    def nn_unet_activation(x):
+        x = InstanceNormalization()(x)
+        x = LeakyReLU(alpha=0.01)(x)
+        return x
 
     def attention_gate_3d(x, g, inter_shape):
         x_theta = Conv3D(filters=inter_shape,
@@ -317,23 +349,32 @@ def create_unet_model_3d(input_image_size,
         if i == 0:
             conv = Conv3D(filters=number_of_filters,
                           kernel_size=convolution_kernel_size,
-                          activation=activation_type,
                           padding='same',
                           kernel_regularizer=regularizers.l2(weight_decay))(inputs)
         else:
             conv = Conv3D(filters=number_of_filters,
                           kernel_size=convolution_kernel_size,
-                          activation=activation_type,
                           padding='same',
                           kernel_regularizer=regularizers.l2(weight_decay))(pool)
+
+        if nn_unet_activation_style == True:
+            conv = nn_unet_activation(conv)
+        else:
+            conv = ReLU()(conv)
 
         if dropout_rate > 0.0:
             conv = Dropout(rate=dropout_rate)(conv)
 
-        encoding_convolution_layers.append(Conv3D(filters=number_of_filters,
-                                                  kernel_size=convolution_kernel_size,
-                                                  activation=activation_type,
-                                                  padding='same')(conv))
+        conv = Conv3D(filters=number_of_filters,
+                      kernel_size=convolution_kernel_size,
+                      padding='same')(conv)
+
+        if nn_unet_activation_style == True:
+            conv = nn_unet_activation(conv)
+        else:
+            conv = ReLU()(conv)
+
+        encoding_convolution_layers.append(conv)
 
         if i < number_of_layers - 1:
             pool = MaxPooling3D(pool_size=pool_size)(encoding_convolution_layers[i])
@@ -347,6 +388,8 @@ def create_unet_model_3d(input_image_size,
                                  kernel_size=deconvolution_kernel_size,
                                  padding='same',
                                  kernel_regularizer=regularizers.l2(weight_decay))(outputs)
+        if nn_unet_activation_style == True:
+            deconv = nn_unet_activation(deconv)
         deconv = UpSampling3D(size=pool_size)(deconv)
 
         if add_attention_gating == True:
@@ -359,18 +402,25 @@ def create_unet_model_3d(input_image_size,
 
         outputs = Conv3D(filters=number_of_filters,
                          kernel_size=convolution_kernel_size,
-                         activation=activation_type,
                          padding='same',
                          kernel_regularizer=regularizers.l2(weight_decay))(outputs)
+        if nn_unet_activation_style == True:
+            outputs = nn_unet_activation(outputs)
+        else:
+            outputs = ReLU()(outputs)
 
         if dropout_rate > 0.0:
             outputs = Dropout(rate=dropout_rate)(outputs)
 
         outputs = Conv3D(filters=number_of_filters,
                          kernel_size=convolution_kernel_size,
-                         activation=activation_type,
                          padding='same',
                          kernel_regularizer=regularizers.l2(weight_decay))(outputs)
+
+        if nn_unet_activation_style == True:
+            outputs = nn_unet_activation(outputs)
+        else:
+            outputs = ReLU()(outputs)
 
     convActivation = ''
 
