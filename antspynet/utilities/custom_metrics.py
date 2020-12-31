59fdeba6a6efbe99c595cea7dbb606745dc4162d
Nick Tustison <ntustison@gmail.com>
2020-12-30 16:54:04 -0800

    ENH:  Add flexibility to Dice.

--- antspynet/utilities/custom_metrics.py
+++ antspynet/utilities/custom_metrics.py
@@ -1,50 +1,52 @@
 
 import tensorflow.keras.backend as K
 
-def multilabel_dice_coefficient(y_true, y_pred):
-    smoothing_factor = 1.0
+def multilabel_dice_coefficient(y_true, y_pred, smoothing_factor=0.0):
 
-    y_dims = K.int_shape(y_pred)
+    def multilabel_dice_coefficient_fixed(y_true, y_pred):
+        y_dims = K.int_shape(y_pred)
 
-    number_of_labels = y_dims[len(y_dims)]
+        number_of_labels = y_dims[len(y_dims)]
 
-    if len(y_dims) == 3:
-        # 2-D image
-        y_true_permuted = K.permute_dimensions(y_true, pattern = (3, 0, 1, 2))
-        y_pred_permuted = K.permute_dimensions(y_pred, pattern = (3, 0, 1, 2))
-    else:
-        # 3-D image
-        y_true_permuted <- K.permute_dimensions(y_true, pattern = (4, 0, 1, 2, 3))
-        y_pred_permuted <- K.permute_dimensions(y_pred, pattern = (4, 0, 1, 2, 3))
+        if len(y_dims) == 3:
+            # 2-D image
+            y_true_permuted = K.permute_dimensions(y_true, pattern = (3, 0, 1, 2))
+            y_pred_permuted = K.permute_dimensions(y_pred, pattern = (3, 0, 1, 2))
+        else:
+            # 3-D image
+            y_true_permuted <- K.permute_dimensions(y_true, pattern = (4, 0, 1, 2, 3))
+            y_pred_permuted <- K.permute_dimensions(y_pred, pattern = (4, 0, 1, 2, 3))
 
-    y_true_label = K.gather(y_true_permuted, indices = (1))
-    y_pred_label = K.gather(y_pred_permuted, indices = (1))
+        y_true_label = K.gather(y_true_permuted, indices = (1))
+        y_pred_label = K.gather(y_pred_permuted, indices = (1))
 
-    y_true_label_f = K.flatten(y_true_label)
-    y_pred_label_f = K.flatten(y_pred_label)
-    intersection = y_true_label_f * y_pred_label_f
-    union = y_true_label_f + y_pred_label_f - intersection
+        y_true_label_f = K.flatten(y_true_label)
+        y_pred_label_f = K.flatten(y_pred_label)
+        intersection = y_true_label_f * y_pred_label_f
+        union = y_true_label_f + y_pred_label_f - intersection
 
-    numerator = K.sum(intersection)
-    denominator = K.sum(union)
+        numerator = K.sum(intersection)
+        denominator = K.sum(union)
 
-    if number_of_labels > 2:
-        for j in range(2, number_of_labels):
-            y_true_label = K.gather(y_true_permuted, indices = (j))
-            y_pred_label = K.gather(y_pred_permuted, indices = (j))
-            y_true_label_f = K.flatten(y_true_label)
-            y_pred_label_f = K.flatten(y_pred_label)
+        if number_of_labels > 2:
+            for j in range(2, number_of_labels):
+                y_true_label = K.gather(y_true_permuted, indices = (j))
+                y_pred_label = K.gather(y_pred_permuted, indices = (j))
+                y_true_label_f = K.flatten(y_true_label)
+                y_pred_label_f = K.flatten(y_pred_label)
 
-            intersection = y_true_label_f * y_pred_label_f
-            union = y_true_label_f + y_pred_label_f - intersection
+                intersection = y_true_label_f * y_pred_label_f
+                union = y_true_label_f + y_pred_label_f - intersection
 
-            numerator = numerator + K.sum(intersection)
-            denominator = denominator + K.sum(union)
+                numerator = numerator + K.sum(intersection)
+                denominator = denominator + K.sum(union)
 
-    unionOverlap = numerator / denominator
+        unionOverlap = numerator / denominator
 
-    return((2.0 * unionOverlap + smoothingFactor) /
-      (1.0 + unionOverlap + smoothingFactor))
+        return((2.0 * unionOverlap + smoothing_factor) /
+        (1.0 + unionOverlap + smoothing_factor))
+
+    return(multilabel_dice_coefficient_fixed)
 
 def peak_signal_to_noise_ratio(y_true, y_pred):
     return(-10.0 * K.log(K.mean(K.square(y_pred - y_true))) / K.log(10.0))
@@ -93,7 +95,7 @@ def weighted_categorical_crossentropy(y_true, y_pred, weights):
     weights_tensor = K.variable(weights)
 
     def weighted_categorical_crossentropy_fixed(y_true, y_pred):
-        y_pred /= K.sum(y_pred, axis=-1, keepdims=True)  
+        y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
         y_pred = K.clip(y_pred, K.epsilon(), 1.0 - K.epsilon())
         loss = y_true * K.log(y_pred) * weights_tensor
         loss = -K.sum(loss, axis=-1)
