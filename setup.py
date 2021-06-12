8923869c10e1c9526a10e8c561211fb42b77b902
stnava <stnava@gmail.com>
2021-06-12 13:45:39 -0400

    ENH: v0.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='antspynet',
-      version='0.0',
+      version='0.1',
       description='A collection of deep learning architectures ported to the python language and tools for basic medical image processing.',
       url='http://github.com/ntustison/antspynet',
       author='Nicholas J. Tustison and Brian B. Avants and Nick Cullen',
