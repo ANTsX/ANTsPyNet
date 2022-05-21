67f572c599c3508e235aca4665866138a6231f2b
Nick Tustison <ntustison@gmail.com>
2022-05-21 09:22:57 -0700

    ENH:  Add E13x5 brain extraction.

--- antspynet/utilities/__init__.py
+++ antspynet/utilities/__init__.py
@@ -59,6 +59,7 @@ from .brain_extraction import brain_extraction
 from .cortical_thickness import cortical_thickness
 from .cortical_thickness import longitudinal_cortical_thickness
 from .histology import arterial_lesion_segmentation
+from .histology import e13x5_brain_extraction
 from .lung_extraction import lung_extraction
 from .white_matter_hyperintensity_segmentation import sysu_media_wmh_segmentation
 from .white_matter_hyperintensity_segmentation import ew_david
