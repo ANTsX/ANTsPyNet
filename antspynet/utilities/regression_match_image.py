BUG:  Masking.

antspynet/utilities/regression_match_image.py
@@ -56,10 +56,10 @@ def regression_match_image(source_image,
 
     poly_features = PolynomialFeatures(degree=poly_order)
     source_intensities_poly = poly_features.fit_transform(source_intensities)
-
     model = LinearRegression()
     model.fit(source_intensities_poly, reference_intensities)
-
+    if mask is not None:
+        source_intensities_poly = poly_features.fit_transform(np.expand_dims((source_image.numpy()).flatten(), axis=1))
     matched_source_intensities = model.predict(source_intensities_poly)
 
     if truncate == True:
@@ -67,11 +67,6 @@ def regression_match_image(source_image,
         max_reference_value = reference_intensities.max()
         matched_source_intensities[matched_source_intensities < min_reference_value] = min_reference_value
         matched_source_intensities[matched_source_intensities > max_reference_value] = max_reference_value
-
-    if mask is not None:
-        source_intensities = np.expand_dims((source_image.numpy()).flatten(), axis=1)
-        source_intensities[np.where(mask_intensities != 0)] = matched_source_intensities
-        matched_source_intensities = source_intensities
 
     matched_source_image = ants.make_image(source_image.shape, matched_source_intensities)
     matched_source_image = ants.copy_image_info(source_image,  matched_source_image)