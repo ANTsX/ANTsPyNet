5356de2d16d9bb2bc745eb2c7b434435e17d2562
Nick Tustison <ntustison@gmail.com>
2023-02-06 09:42:28 -0800

    WIP:  More debugging.

--- antspynet/utilities/custom_convolution_layers.py
+++ antspynet/utilities/custom_convolution_layers.py
@@ -81,26 +81,19 @@ class PartialConv2D(Conv2D):
         )
 
         # Apply convolutions to image
-        # image_output = K.conv2d(
-        #     (images*masks), self.kernel,
-        #     strides=self.strides,
-        #     padding='valid',
-        #     data_format=self.data_format,
-        #     dilation_rate=self.dilation_rate
-        # )
         image_output = K.conv2d(
-            images, self.kernel,
+            (images*masks), self.kernel,
             strides=self.strides,
             padding='valid',
             data_format=self.data_format,
             dilation_rate=self.dilation_rate
         )
 
-        # Calculate the mask ratio on each pixel in the output mask
-        mask_ratio = self.window_size / (mask_output + 1e-8)
-
         # Clip output to be between 0 and 1
         mask_output = K.clip(mask_output, 0, 1)
+
+        # Calculate the mask ratio on each pixel in the output mask
+        mask_ratio = self.window_size / (mask_output + 1e-8)
 
         # Remove ratio values where there are holes
         mask_ratio = mask_ratio * mask_output
@@ -119,148 +112,7 @@ class PartialConv2D(Conv2D):
         if self.activation is not None:
             image_output = self.activation(image_output)
 
-        return [image_output, mask_output]
-
-    def compute_output_shape(self, input_shape):
-        if self.data_format == 'channels_last':
-            space = input_shape[0][1:-1]
-            new_space = []
-            for i in range(len(space)):
-                new_dim = conv_utils.conv_output_length(
-                    space[i],
-                    self.kernel_size[i],
-                    padding='same',
-                    stride=self.strides[i],
-                    dilation=self.dilation_rate[i])
-                new_space.append(new_dim)
-            new_shape = (input_shape[0][0],) + tuple(new_space) + (self.filters,)
-            return [new_shape, new_shape]
-        elif self.data_format == 'channels_first':
-            space = input_shape[2:]
-            new_space = []
-            for i in range(len(space)):
-                new_dim = conv_utils.conv_output_length(
-                    space[i],
-                    self.kernel_size[i],
-                    padding='same',
-                    stride=self.strides[i],
-                    dilation=self.dilation_rate[i])
-                new_space.append(new_dim)
-            new_shape = (input_shape[0], self.filters) + tuple(new_space)
-            return [new_shape, new_shape]
-
-class PartialConv3D(Conv3D):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.input_spec = [InputSpec(ndim=5), InputSpec(ndim=5)]
-
-    def build(self, input_shape):
-        if self.data_format == 'channels_first':
-            channel_axis = 1
-        else:
-            channel_axis = -1
-
-        if input_shape[0][channel_axis] is None:
-            raise ValueError('The channel dimension of the inputs should be defined. Found `None`.')
-
-        self.input_dim = input_shape[0][channel_axis]
-
-        # Image kernel
-        kernel_shape = self.kernel_size + (self.input_dim, self.filters)
-        self.kernel = self.add_weight(shape=kernel_shape,
-                                      initializer=self.kernel_initializer,
-                                      name='image_kernel',
-                                      regularizer=self.kernel_regularizer,
-                                      constraint=self.kernel_constraint,
-                                      trainable=True)
-        # Mask kernel
-        self.kernel_mask = self.add_weight(shape=kernel_shape,
-                                      initializer='ones',
-                                      name='mask_kernel',
-                                      regularizer=self.kernel_regularizer,
-                                      constraint=self.kernel_constraint,
-                                      trainable=False)
-
-        # Calculate padding size to achieve zero-padding
-        self.pconv_padding = (
-            (int((self.kernel_size[0]-1)/2), int((self.kernel_size[0]-1)/2)),
-            (int((self.kernel_size[1]-1)/2), int((self.kernel_size[1]-1)/2)),
-            (int((self.kernel_size[2]-1)/2), int((self.kernel_size[2]-1)/2))
-        )
-
-        # Window size - used for normalization
-        self.window_size = self.kernel_size[0] * self.kernel_size[1] * self.kernel_size[2]
-
-        if self.use_bias:
-            self.bias = self.add_weight(shape=(self.filters,),
-                                        initializer=self.bias_initializer,
-                                        name='bias',
-                                        regularizer=self.bias_regularizer,
-                                        constraint=self.bias_constraint,
-                                        trainable=True)
-        else:
-            self.bias = None
-        self.built = True
-
-    def call(self, inputs, mask=None):
-        '''
-        We will be using the Keras conv3d method, and essentially we have
-        to do here is multiply the mask with the input X, before we apply the
-        convolutions. For the mask itself, we apply convolutions with all weights
-        set to 1. Subsequently, we clip mask values to between 0 and 1.
-        '''
-
-        # Both image and mask must be supplied
-        if type(inputs) is not list or len(inputs) != 2:
-            raise Exception('PartialConvolution3D must be called on a list of two tensors [img, mask]. Instead got: ' + str(inputs))
-
-        # Padding done explicitly so that padding becomes part of the masked partial convolution
-        images = K.spatial_3d_padding(inputs[0], self.pconv_padding, self.data_format)
-        masks = K.spatial_3d_padding(inputs[1], self.pconv_padding, self.data_format)
-
-        # Apply convolutions to mask
-        mask_output = K.conv3d(
-            masks, self.kernel_mask,
-            strides=self.strides,
-            padding='valid',
-            data_format=self.data_format,
-            dilation_rate=self.dilation_rate
-        )
-
-        # Apply convolutions to image
-        image_output = K.conv3d(
-            (images*masks), self.kernel,
-            strides=self.strides,
-            padding='valid',
-            data_format=self.data_format,
-            dilation_rate=self.dilation_rate
-        )
-
-        # Calculate the mask ratio on each pixel in the output mask
-        mask_ratio = self.window_size / (mask_output + 1e-8)
-
-        # Clip output to be between 0 and 1
-        mask_output = K.clip(mask_output, 0, 1)
-
-        # Remove ratio values where there are holes
-        mask_ratio = mask_ratio * mask_output
-
-        # Normalize iamge output
-        image_output = image_output * mask_ratio
-
-        # Apply bias only to the image (if chosen to do so)
-        if self.use_bias:
-            image_output = K.bias_add(
-                image_output,
-                self.bias,
-                data_format=self.data_format)
-
-        # Apply activations on the image
-        if self.activation is not None:
-            image_output = self.activation(image_output)
-
-        return [image_output, mask_output]
+        return [image_output, mask_ratio]
 
     def compute_output_shape(self, input_shape):
         if self.data_format == 'channels_last':
