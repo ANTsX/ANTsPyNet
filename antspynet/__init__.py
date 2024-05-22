47171102c720ea0672c9ee5900a4a3ee6b766365
stnava <stnava@gmail.com>
2024-05-22 13:01:44 -0400

    WIP: importlib experiment

--- antspynet/__init__.py
+++ antspynet/__init__.py
@@ -1,6 +1,8 @@
-from importlib.metadata import version
+import importlib
+modules = ['architectures', 'utilities']
+# Import each module dynamically
+for module in modules:
+    importlib.import_module(f'.{module}', package=__name__)
 
-__version__ = version('antspynet')
-
-from .architectures import *
-from .utilities import *
+# Define the public API
+__all__ = ['architectures', 'utilities']
