eccac5776fc17f8e242e818a000dd919c614fc98
stnava <stnava@gmail.com>
2024-05-22 13:27:57 -0400

    WIP: automate submodule discovery

--- antspynet/__init__.py
+++ antspynet/__init__.py
@@ -1,7 +1,21 @@
 import importlib
-modules = ['architectures', 'utilities']
+import pkgutil
 
-from .architectures import *
-from .utilities import *
+modules = ['antspynet.architectures', 'antspynet.utilities']
 
-__all__ = ['architectures', 'utilities']
+# Initialize an empty list to hold all functions
+__all__ = []
+
+def import_submodules(package_name):
+    package = importlib.import_module(package_name)
+    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__, package.__name__ + '.'):
+        module = importlib.import_module(name)
+        for attribute_name in dir(module):
+            attribute = getattr(module, attribute_name)
+            if callable(attribute):
+                globals()[attribute_name] = attribute
+                __all__.append(attribute_name)
+
+# Import all submodules and their functions
+for x in range(len(modules)):
+    import_submodules(modules[x])
