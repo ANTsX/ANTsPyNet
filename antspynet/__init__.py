07c7768019d6b645a6cd9df8ff546d031e3d6eb9
stnava <stnava@gmail.com>
2024-05-22 13:35:37 -0400

    WIP: automate submodule discovery - x4

--- antspynet/__init__.py
+++ antspynet/__init__.py
@@ -1,13 +1,24 @@
+# mypackage/__init__.py
+
 import importlib
 import pkgutil
+import sys
 
 # Initialize an empty list to hold all functions
 __all__ = []
 
 def import_submodules(package_name):
-    package = importlib.import_module(package_name)
+    try:
+        package = importlib.import_module(package_name)
+    except ModuleNotFoundError as e:
+        print(f"Error importing package {package_name}: {e}", file=sys.stderr)
+        return
     for loader, name, is_pkg in pkgutil.walk_packages(package.__path__, package.__name__ + '.'):
-        module = importlib.import_module(name)
+        try:
+            module = importlib.import_module(name)
+        except ModuleNotFoundError as e:
+            print(f"Error importing module {name}: {e}", file=sys.stderr)
+            continue
         for attribute_name in dir(module):
             attribute = getattr(module, attribute_name)
             if callable(attribute):
