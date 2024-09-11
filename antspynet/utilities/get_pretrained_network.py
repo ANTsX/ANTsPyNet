4db3fd8bb53d727858a6b0cf7ae6efa00047a671
Nick Tustison <ntustison@gmail.com>
2024-09-11 07:29:07 -0700

    ENH:  Add network weights.

--- antspynet/utilities/get_pretrained_network.py
+++ antspynet/utilities/get_pretrained_network.py
@@ -171,10 +171,10 @@ def get_pretrained_network(file_id=None,
             "tb_antsxnet": "https://figshare.com/ndownloader/files/45820599",
             "wholeTumorSegmentationT2Flair": "https://ndownloader.figshare.com/files/14087045",
             "wholeLungMaskFromVentilation": "https://ndownloader.figshare.com/files/28914441",
-            "DeepAtroposHcpT1Weights": "",
-            "DeepAtroposHcpT1T2Weights": "",
-            "DeepAtroposHcpT1FAWeights": "",
-            "DeepAtroposHcpT1T2FAWeights": ""
+            "DeepAtroposHcpT1Weights": "https://figshare.com/ndownloader/files/49132504",
+            "DeepAtroposHcpT1T2Weights": "https://figshare.com/ndownloader/files/49132498",
+            "DeepAtroposHcpT1FAWeights": "https://figshare.com/ndownloader/files/49132507",
+            "DeepAtroposHcpT1T2FAWeights": "https://figshare.com/ndownloader/files/49132501"
         }
         return(switcher.get(argument, "Invalid argument."))
 
