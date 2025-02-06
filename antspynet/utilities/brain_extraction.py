39efbca22669900eacd06142a1fbd48df1530a54
Nick Tustison <ntustison@gmail.com>
2025-02-06 10:30:30 -0800

    ENH:  Extend FOV

--- antspynet/utilities/brain_extraction.py
+++ antspynet/utilities/brain_extraction.py
@@ -180,9 +180,9 @@ def brain_extraction(image,
             reorient_template_mask = ants.image_read(get_antsxnet_data("hcpyaTemplateBrainMask"))
             reorient_template = reorient_template * reorient_template_mask
             reorient_template = ants.resample_image(reorient_template, (1, 1, 1), use_voxels=False, interp_type=0)
-            reorient_template = pad_or_crop_image_to_size(reorient_template, (160, 176, 160))
+            reorient_template = pad_or_crop_image_to_size(reorient_template, (160, 192, 160))
             xfrm = ants.create_ants_transform(transform_type="Euler3DTransform",
-                center=np.asarray(ants.get_center_of_mass(reorient_template)), translation=(0, -10, -15))
+                center=np.asarray(ants.get_center_of_mass(reorient_template)), translation=(0, 0, 0))
             reorient_template = xfrm.apply_to_image(reorient_template)
         else:
             reorient_template = ants.image_read(get_antsxnet_data("S_template3"))
