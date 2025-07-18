a52567ba3a1f639ccede0a520d3bc89f061d2478
ntustison <ntustison@gmail.com>
2025-07-18 10:23:04 -0600

    BUG:  Rm brain ext tests.

--- tests/test_brain_extraction.py
+++ tests/test_brain_extraction.py
@@ -20,14 +20,14 @@ class Test_t1(unittest.TestCase):
 #         t1 = ants.image_read(antspynet.get_antsxnet_data('mprage_hippmapp3r'))
 #         seg = antspynet.brain_extraction(t1, modality="t1nobrainer")
 
-class Test_t1combined(unittest.TestCase):
-    def setUp(self):
-        pass
-    def tearDown(self):
-        pass
-    def test_example(self):
-        t1 = ants.image_read(antspynet.get_antsxnet_data('mprage_hippmapp3r'))
-        seg = antspynet.brain_extraction(t1, modality="t1combined")
+# class Test_t1combined(unittest.TestCase):
+#     def setUp(self):
+#         pass
+#     def tearDown(self):
+#         pass
+#     def test_example(self):
+#         t1 = ants.image_read(antspynet.get_antsxnet_data('mprage_hippmapp3r'))
+#         seg = antspynet.brain_extraction(t1, modality="t1combined")
 
 # class Test_t1threetissue(unittest.TestCase):
 #     def setUp(self):
@@ -38,23 +38,23 @@ class Test_t1combined(unittest.TestCase):
 #         t1 = ants.image_read(antspynet.get_antsxnet_data('mprage_hippmapp3r'))
 #         bext = antspynet.brain_extraction(t1, modality="t1threetissue")
 
-class Test_t1hemi(unittest.TestCase):
-    def setUp(self):
-        pass
-    def tearDown(self):
-        pass
-    def test_example(self):
-        t1 = ants.image_read(antspynet.get_antsxnet_data('mprage_hippmapp3r'))
-        bext = antspynet.brain_extraction(t1, modality="t1hemi")
+# class Test_t1hemi(unittest.TestCase):
+#     def setUp(self):
+#         pass
+#     def tearDown(self):
+#         pass
+#     def test_example(self):
+#         t1 = ants.image_read(antspynet.get_antsxnet_data('mprage_hippmapp3r'))
+#         bext = antspynet.brain_extraction(t1, modality="t1hemi")
 
-class Test_t1lobes(unittest.TestCase):
-    def setUp(self):
-        pass
-    def tearDown(self):
-        pass
-    def test_example(self):
-        t1 = ants.image_read(antspynet.get_antsxnet_data('mprage_hippmapp3r'))
-        bext = antspynet.brain_extraction(t1, modality="t1lobes")
+# class Test_t1lobes(unittest.TestCase):
+#     def setUp(self):
+#         pass
+#     def tearDown(self):
+#         pass
+#     def test_example(self):
+#         t1 = ants.image_read(antspynet.get_antsxnet_data('mprage_hippmapp3r'))
+#         bext = antspynet.brain_extraction(t1, modality="t1lobes")
 
 if __name__ == '__main__':
     unittest.main()
