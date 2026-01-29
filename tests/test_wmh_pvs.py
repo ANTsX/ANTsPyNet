7071a46ba47b18f76fe2d1714e6578d2c007d611
Philip A Cook <cookpa@pennmedicine.upenn.edu>
2026-01-29 16:42:47 -0500

    CI: Update figshare URL

--- tests/test_wmh_pvs.py
+++ tests/test_wmh_pvs.py
@@ -9,9 +9,9 @@ class Test_sysu_wmh(unittest.TestCase):
     def tearDown(self):
         pass
     def test_example(self):
-        t1_file = tf.keras.utils.get_file(fname="t1.nii.gz", origin="https://figshare.com/ndownloader/files/40251796")
+        t1_file = tf.keras.utils.get_file(fname="t1.nii.gz", origin="https://ndownloader.figshare.com/files/40251796")
         t1 = ants.image_read(t1_file)
-        flair_file = tf.keras.utils.get_file(fname="flair.nii.gz", origin="https://figshare.com/ndownloader/files/40251793")
+        flair_file = tf.keras.utils.get_file(fname="flair.nii.gz", origin="https://ndownloader.figshare.com/files/40251793")
         flair = ants.image_read(flair_file)
         wmh = antspynet.sysu_media_wmh_segmentation(flair, t1)
 
@@ -21,9 +21,9 @@ class Test_hypermapp3r_wmh(unittest.TestCase):
     def tearDown(self):
         pass
     def test_example(self):
-        t1_file = tf.keras.utils.get_file(fname="t1.nii.gz", origin="https://figshare.com/ndownloader/files/40251796")
+        t1_file = tf.keras.utils.get_file(fname="t1.nii.gz", origin="https://ndownloader.figshare.com/files/40251796")
         t1 = ants.image_read(t1_file)
-        flair_file = tf.keras.utils.get_file(fname="flair.nii.gz", origin="https://figshare.com/ndownloader/files/40251793")
+        flair_file = tf.keras.utils.get_file(fname="flair.nii.gz", origin="https://ndownloader.figshare.com/files/40251793")
         flair = ants.image_read(flair_file)
         wmh = antspynet.hypermapp3r_segmentation(t1, flair)
 
@@ -33,9 +33,9 @@ class Test_shiva_wmh(unittest.TestCase):
     def tearDown(self):
         pass
     def test_example(self):
-        t1_file = tf.keras.utils.get_file(fname="t1.nii.gz", origin="https://figshare.com/ndownloader/files/40251796")
+        t1_file = tf.keras.utils.get_file(fname="t1.nii.gz", origin="https://ndownloader.figshare.com/files/40251796")
         t1 = ants.image_read(t1_file)
-        flair_file = tf.keras.utils.get_file(fname="flair.nii.gz", origin="https://figshare.com/ndownloader/files/40251793")
+        flair_file = tf.keras.utils.get_file(fname="flair.nii.gz", origin="https://ndownloader.figshare.com/files/40251793")
         flair = ants.image_read(flair_file)
         wmh = antspynet.shiva_wmh_segmentation(flair, t1, which_model="all")
 
@@ -45,10 +45,10 @@ class Test_shiva_wmh(unittest.TestCase):
 #     def tearDown(self):
 #         pass
 #     def test_example(self):
-#         t1_file = tf.keras.utils.get_file(fname="t1.nii.gz", origin="https://figshare.com/ndownloader/files/40251796")
+#         t1_file = tf.keras.utils.get_file(fname="t1.nii.gz", origin="https://ndownloader.figshare.com/files/40251796")
 #         t1 = ants.image_read(t1_file)
 #         t1 = ants.resample_image(t1, (240, 240, 96), use_voxels=True)
-#         flair_file = tf.keras.utils.get_file(fname="flair.nii.gz", origin="https://figshare.com/ndownloader/files/40251793")
+#         flair_file = tf.keras.utils.get_file(fname="flair.nii.gz", origin="https://ndownloader.figshare.com/files/40251793")
 #         flair = ants.image_read(flair_file)
 #         flair = ants.resample_image(flair, (240, 240, 96), use_voxels=True)
 #         wmh = antspynet.wmh_segmentation(flair, t1, use_combined_model=True)
@@ -59,9 +59,9 @@ class Test_shiva_wmh(unittest.TestCase):
     def tearDown(self):
         pass
     def test_example(self):
-        t1_file = tf.keras.utils.get_file(fname="pvs_t1.nii.gz", origin="https://figshare.com/ndownloader/files/48675367")
+        t1_file = tf.keras.utils.get_file(fname="pvs_t1.nii.gz", origin="https://ndownloader.figshare.com/files/48675367")
         t1 = ants.image_read(t1_file)
-        flair_file = tf.keras.utils.get_file(fname="pvs_flair.nii.gz", origin="https://figshare.com/ndownloader/files/48675352")
+        flair_file = tf.keras.utils.get_file(fname="pvs_flair.nii.gz", origin="https://ndownloader.figshare.com/files/48675352")
         flair = ants.image_read(flair_file)
         pvs = antspynet.shiva_pvs_segmentation(t1, flair, which_model = "all")
 
